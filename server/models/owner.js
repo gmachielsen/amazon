bd20726613ccde7c8f35f1b1c45ca74994d6d24f
gmachielsen <g.machielsen@gmail.com>
2020-03-01 21:34:00 +0100

    initial commit

---
+++ server/models/owner.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const OwnerSchema = new Schema({
+    title: String,
+    about: String,
+    photo: String
+   
+});
+
+module.exports = mongoose.model("Owner", OwnerSchema);
